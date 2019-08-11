13076dafbb0577f643550f6bd6079f0298abe163
amirahmadadibi <amirahmadadibi@gmail.com>
2019-08-11 10:52:47 +0430

    initialziation of the project

---
+++ app/src/main/java/com/amirahmad/projects/monaddesignpattern/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.amirahmad.projects.monaddesignpattern
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
